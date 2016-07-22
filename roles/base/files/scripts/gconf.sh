6beea5d7bd514faa54e0a92f54b35a99137ee27b
Joseph Harry <findarato@gmail.com>
2016-07-22 15:18:06 -0500

    Finally got minecraft launching to work

--- roles/base/files/scripts/gconf.sh
+++ roles/base/files/scripts/gconf.sh
@@ -11,5 +11,11 @@ gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/foreground
 gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/background_color "black"
 ### Adjust screensaver settings
 gconftool-2 --type bool --set /org/gnome/desktop/screensaver/lock-enabled False
-gconftool-2 --type string --set /org/gnome/desktop/session/idle-delay "0"
+gconftool-2 --type integer --set /org/gnome/desktop/screensaver/lock-delay 0
+gconftool-2 --type integer --set /org/gnome/desktop/screensaver/ubuntu-lock-on-suppend False
 
+gconftool-2 --type integer --set /org/gnome/desktop/session/idle-delay 300
+gconftool-2 --type bool --set /org/gnome/desktop/screensaver/idle-activation-enabled True
+
+### Adjust Wallpaper Settings
+gconftool-2 --type string --set /org/gnome/desktop/background/picture-uri "file:///usr/share/backgrounds/publicClassroom.jpg"
---
+++ roles/minecraftStart/files/startMinecraft.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DISPLAY=:0
+screen -S Minecraft /usr/bin/java -jar /opt/minecraft/Minecraft.jar -d -m
