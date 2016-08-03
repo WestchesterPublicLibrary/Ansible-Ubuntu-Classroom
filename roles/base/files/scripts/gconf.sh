5739c4bf171b7fc12e65f5719ca34c4d7c622a49
Joseph Harry <findarato@gmail.com>
2016-08-02 19:34:35 -0500

    fonts installed

--- roles/base/files/scripts/gconf.sh
+++ roles/base/files/scripts/gconf.sh
@@ -1,21 +1,30 @@
 ### Gconftool settings
 # Adjust terminal Settings
-gconftool-2 --type bool --set /apps/gnome-terminal/profiles/Default/use_system_font False
-gconftool-2 --type bool --set /apps/gnome-terminal/profiles/Default/use_theme_colors False
-gconftool-2 --type bool --set /apps/gnome-terminal/profiles/Default/scrollback_unlimited True
-gconftool-2 --type bool --set /apps/gnome-terminal/profiles/Default/default_show_menubar False
-gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/font "Droid Sans Mono 8"
+gconftool-2 --type boolean --set /apps/gnome-terminal/profiles/Default/use_system_font false
+gconftool-2 --type boolean --set /apps/gnome-terminal/profiles/Default/use_theme_colors false
+gconftool-2 --type boolean --set /apps/gnome-terminal/profiles/Default/scrollback_unlimited true
+gconftool-2 --type boolean --set /apps/gnome-terminal/profiles/Default/default_show_menubar false
+gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/font "Hack Regular 10"
 gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/scrollbar_position "hidden"
 gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/background_type "transparent"
 gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/foreground_color "white"
 gconftool-2 --type string --set /apps/gnome-terminal/profiles/Default/background_color "black"
+
+gconftool-2 --type boolean --set /con/cononical/unity/integrated-menus true
+gconftool-2 --type as --set /com/cononical/unity/webapps []
+gconftool-2 --type as --set /org/gnome/desktop/wm/preferences ["Default","Second","Cool","Beans"]
+
 ### Adjust screensaver settings
-gconftool-2 --type bool --set /org/gnome/desktop/screensaver/lock-enabled False
+gconftool-2 --type boolean --set /org/gnome/desktop/screensaver/lock-enabled false
 gconftool-2 --type integer --set /org/gnome/desktop/screensaver/lock-delay 0
-gconftool-2 --type integer --set /org/gnome/desktop/screensaver/ubuntu-lock-on-suppend False
+gconftool-2 --type boolean --set /org/gnome/desktop/screensaver/ubuntu-lock-on-suppend false
 
 gconftool-2 --type integer --set /org/gnome/desktop/session/idle-delay 300
-gconftool-2 --type bool --set /org/gnome/desktop/screensaver/idle-activation-enabled True
+gconftool-2 --type boolean --set /org/gnome/desktop/screensaver/idle-activation-enabled true
 
 ### Adjust Wallpaper Settings
-gconftool-2 --type string --set /org/gnome/desktop/background/picture-uri "file:///usr/share/backgrounds/publicClassroom.jpg"
+gconftool-2 --type string --set /org/gnome/desktop/background/picture-uri "https://wpl.lib.in.us/logos/WPL_Logo_2016/WPL%20Logo%20Web%20Version/WPL_logo_web_700px.png"
+
+
+gconftool-2 --type string --set /org/gnome/desktop/interface/monospace-font-name 'Hack Regular 12'
+gconftool-2 --type string --set /org/gnome/nautilus/desktop/font 'Ubuntu 10'
