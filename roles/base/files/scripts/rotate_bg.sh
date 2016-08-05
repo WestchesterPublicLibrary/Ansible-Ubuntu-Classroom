ba381be9693f171848c74a45106a3c4de6749c1f
Joseph Harry <findarato@gmail.com>
2016-08-05 12:31:20 -0500

    Hopefully the wallpaper change actually works

--- roles/base/files/scripts/rotate_bg.sh
+++ roles/base/files/scripts/rotate_bg.sh
@@ -6,6 +6,6 @@
 # https://major.io/2015/02/11/rotate-gnome-3s-wallpaper-systemd-user-units-timers/
 #
 
-walls_dir=/opt/provision/wallpaper/$HOME
+walls_dir=/opt/provision/wallpaper/$USER
 selection=$(find $walls_dir -type f -name "*.jpg" -o -name "*.png" | shuf -n1)
 gsettings set org.gnome.desktop.background picture-uri "file://$selection"
